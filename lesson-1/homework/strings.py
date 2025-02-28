5399e05c439788a635b8dd2cb86d41b10552993d
Muhammadjon091 <113112878+Muhammadjon091@users.noreply.github.com>
2025-02-28 11:25:56 +0500

    Create strings.py

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,27 @@
+a = int(input("Eni: "))
+b = int(input("Bo'yi: "))
+p = 2 * (a + b)
+print(p)
+
+
+d=int(input("Diametr: "))
+l=3.14*d
+print(l)
+
+
+
+
+a = int(input("a sonni kiriting: "))
+b = int(input("b sonni kiriting: "))
+m = (a + b) / 2
+print(m)
+
+
+
+a = int(input("a sonni kiriting: "))
+b = int(input("b sonni kiriting: "))
+s = a + b
+p = a * b
+asq = a ** 2 
+bsq = b ** 2
+print(s, p, asq, bsq)
