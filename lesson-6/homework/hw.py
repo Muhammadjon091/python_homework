a836e916c1e3912478eb9d45d3eaaa89dd3d95ee
Muhammadjon091 <113112878+Muhammadjon091@users.noreply.github.com>
2025-03-22 18:01:31 +0500

    Update hw.py

--- lesson-6/homework/hw.py
+++ lesson-6/homework/hw.py
@@ -46,3 +46,89 @@ for i in range(1, 6):
     print()  # Move to the next line
 
 
+def sum_of_numbers(n):
+    return n * (n + 1) // 2  # Using integer division
+# Example usage
+num = int(input("Enter a number: "))
+print("Sum from 1 to", num, "is:", sum_of_numbers(num))
+
+
+def multiplication_table(n, up_to=10):
+    for i in range(1, up_to + 1):
+        print(f"{n} x {i} = {n * i}")
+# Example usage
+num = int(input("Enter a number: "))
+multiplication_table(num)
+
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+# Indekslar bo‘yicha kerakli elementlarni chiqarish
+indekslar = [1, 2, 4]  # 75, 150 va 145 joylashgan indekslar
+for i in indekslar:
+    print(numbers[i])
+
+
+num = 75869
+print("Total digits:", len(str(num)))
+
+
+n = 5
+for i in range(n, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+
+list1 = [10, 20, 30, 40, 50]
+for num in reversed(list1):
+    print(num)
+
+
+for num in range(-10, 0):
+    print(num)
+
+
+
+for i in range(5):
+    print(i)
+print("Done!")
+
+
+
+start, end = 25, 50
+print("Prime numbers between", start, "and", end, ":")
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+
+
+n = 10
+a, b = 0, 1
+print("Fibonacci sequence:")
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+
+
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+num = 5
+print(f"{num}! =", factorial(num))
+
+
+def uncommon_elements(list1, list2):
+    return [item for item in list1 + list2 if (list1 + list2).count(item) == 1]
+# Test
+print(uncommon_elements([1, 1, 2], [2, 3, 4]))  # [1, 1, 3, 4]
+print(uncommon_elements([1, 2, 3], [4, 5, 6]))  # [1, 2, 3, 4, 5, 6]
+print(uncommon_elements([1, 1, 2, 3, 4, 2], [1, 3, 4, 5]))  # [2, 2, 5]
+
+
